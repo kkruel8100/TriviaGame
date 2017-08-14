31fa0a3afc011840a1eca81e485fa09ee017d989
kkruel8100 <kkruel.kruel1@gmail.com>
2017-08-13 18:31:28 -0700

    Fixed timer_reset_empty

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,25 +17,25 @@ var trivia = [{
 	}	
 ];
 
-var time = 4; //variable for countdown per question.  if change time changes, update here and in reset function
+var time = 8; //variable for countdown per question.  if change time changes, update here and in reset function
 var interval; //variable for countdown by 1 second increments
 var unanswered = 0; //variable for unanswered questions
 var currentQuestion = false;
 var correct = 0;
 var incorrect = 0;
 var i=0;
-
+var gameOver = false;
 
 $("#start").on("click", start);
 $("#restart").on("click", reset);
 
 	function start () {
 		getQuestion(i);
-		countdown(time);
+		// countdown(time);
 	}
 
 	function reset () {
-		countdown(4);
+		countdown(8);
 	}
 
 	//function to countdown by 1 second increments
@@ -54,18 +54,21 @@ $("#restart").on("click", reset);
 			unanswered++;
 			console.log(unanswered);
 			$("#game").text("The correct answer is " + trivia[i].answer + ".");
+			$("#timer").hide();
+			console.log(trivia[i].answer);
+			setTimeout(empty, 4000);
+			// countdown (4);
+				// if (time===0) {
+					// stop();
+					// empty();
+				// }
 	//		i++;
 	//		currentQuestion=false;
-			empty();
+	//		empty();
 	//		console.log("what is i" + i);
 	//		console.log(trivia.length);
-	//		console.log(time);
-			
-			
+	//		console.log(time);		
 		}
-	
-	
-
 	}
 
 	//function to stop the countdown 
@@ -73,44 +76,42 @@ $("#restart").on("click", reset);
 		clearInterval(interval);
 	}
 
-
-
-
 	function getQuestion (i) {
-		if (currentQuestion===false) {
-		countdown(4);
-		$("#game").append("What was " + trivia[i].question + " original group name?");
-			
+		if (i<trivia.length && gameOver===false) {
+		countdown(8);
+		$("#game").append("What was " + trivia[i].question + " original group name?");			
 		console.log(trivia[i].question);
 		console.log(trivia[i].choices.length);
 			for (a=0; a<trivia[i].choices.length; a++) {
 				$("#game").append("<p>" + trivia[i].choices[a] + "</p>" );
-
 			}
 		currentQuestion===true;	
-		}		
+		}	
+		else {
+			gameOver=true;
+		}	
 	}//get question
 
-
-
-	function empty () {
-		if (i<trivia.length) {
+	function empty () {		
 		console.log("what is i in empty " + i);
 		console.log(currentQuestion);
-//		$("#timer").hide();
-		i++;
-		$("#game").empty();
- 		time=4;
-		console.log(time);
-		getQuestion(i);
-//		countdown(time);
-		}
-		else {
-				stop();
-				console.log("Game over");
-			}
-
+		$("#timer").show();
 		
+ 		time=4;
+ 		i++;
+		console.log(time);
+		console.log(i);
+		// countdown(time);
+		// if (time===0) {
+			 $("#game").empty();
+			// i++;
+			// console.log("What is i++ " + i);
+			getQuestion(i);
+		// }	
+		// if (i=trivia.length) {
+				// stop();
+				// console.log("Game over");
+			// }
 	}
 
 });//document ready
